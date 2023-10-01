1a5356c7fb74f99b24ecea2e436711f9c2a7afe5
Marco Túlio Santana Nascimento <marco_nascimento@hst.br>
2023-10-01 11:22:31 -0300

    Display products in table

---
+++ api/DisplayProducts.php
@@ -0,0 +1,46 @@
+<?php
+require_once 'connection.php';
+
+$query = "SELECT * FROM Products WHERE active=1";
+$result = $db->query($query);
+
+if ($result) {
+    echo '<div class="card-body">';
+    while ($row = $result->fetch_assoc()) {
+        echo '<div class="table-responsive">';
+            echo '<table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">';
+                echo '<thead>';
+                    echo '<tr>';
+                        echo '<th>Produto</th>';
+                        echo '<th>Categoria</th>';
+                        echo '<th>Preço de custo</th>';
+                        echo '<th>Preço de venda</th>';
+                        echo '<th>Porcentagem de lucro</th>';
+                        echo '<th>Ativo</th>';
+                    echo '</tr>';
+                echo '</thead>';
+        echo '<tbody>';
+        
+        // Calcula a porcentagem de lucro (30%)
+        $precoCusto = $row['price'];
+        $porcentagemLucro = 30;
+        $precoVenda = $precoCusto + ($precoCusto * ($porcentagemLucro / 100));
+
+        echo '<tr>';
+        echo '<td>' . $row['nameProduct'] . '</td>';
+        echo '<td>' . $row['categoryName'] . '</td>';
+        echo '<td>' . $precoCusto . '</td>';
+        echo '<td>' . $precoVenda . '</td>';
+        echo '<td>' . $porcentagemLucro . '%' . '</td>';
+        echo '<td>' . $row['active'] . '</td>';
+        echo '</tr>';
+        echo '</tbody>';
+        echo '</table>';
+        echo '</div>';
+    }
+    echo '</div>';
+} 
+
+$db->close();
+
+?>
