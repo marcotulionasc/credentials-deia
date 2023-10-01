ba0f8c424d7340325198ce15fdb906b9f6cea789
Marco Túlio Santana Nascimento <marco_nascimento@hst.br>
2023-10-01 14:14:13 -0300

    mantenho a postura orgulho da minha quebrada

--- api/DisplayProducts.php
+++ api/DisplayProducts.php
@@ -4,18 +4,21 @@ require_once 'connection.php';
 $query = "SELECT * FROM Products WHERE active=1";
 $result = $db->query($query);
 
+    echo '<div class="card-body">';
+    echo '  <div class="table-responsive">';
+    echo '      <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">';
 if ($result) {
 
-    echo '<thead>';
-    echo '<tr>';
-    echo '<th>Produto</th>';
-    echo '<th>Categoria</th>';
-    echo '<th>Preço de custo</th>';
-    echo '<th>Preço de venda</th>';
-    echo '<th>Porcentagem de lucro</th>';
-    echo '<th>Ativo</th>';
-    echo '</tr>';
-    echo '</thead>';
+    echo '          <thead>';
+    echo '              <tr>';
+    echo '                  <th>Produto</th>';
+    echo '                  <th>Categoria</th>';
+    echo '                  <th>Preço de custo</th>';
+    echo '                  <th>Preço de venda</th>';
+    echo '                  <th>Porcentagem de lucro</th>';
+    echo '                  <th>Ativo</th>';
+    echo '              </tr>';
+    echo '          </thead>';
     echo '<tbody>';
 
     while ($row = $result->fetch_assoc()) {
@@ -39,9 +42,13 @@ if ($result) {
         echo '<td>' . $porcentagemLucro . '%' . '</td>';
         echo '<td>' . $status . '</td>';
         echo '</tr>';
+        echo '</tbody>';
     }
 
-    echo '</tbody>';
+    
+    echo '</table>';
+    echo '</div>';
+    echo '</div>';
 }
 
 $db->close();
