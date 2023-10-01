df2bf1b0b29310758a5489698900ce08b9db0bdd
Marco Túlio Santana Nascimento <marco_nascimento@hst.br>
2023-10-01 14:03:47 -0300

    structure of table html

--- api/DisplayProducts.php
+++ api/DisplayProducts.php
@@ -5,21 +5,6 @@ $query = "SELECT * FROM Products WHERE active=1";
 $result = $db->query($query);
 
 if ($result) {
-    echo '<div class="card-body">';
-    echo '<div class="table-responsive">';
-    echo '<table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">';
-    echo '<thead>';
-    echo '<tr>';
-    echo '<th>Produto</th>';
-    echo '<th>Categoria</th>';
-    echo '<th>Preço de custo</th>';
-    echo '<th>Preço de venda</th>';
-    echo '<th>Porcentagem de lucro</th>';
-    echo '<th>Ativo</th>';
-    echo '</tr>';
-    echo '</thead>';
-    echo '<tbody>';
-
     while ($row = $result->fetch_assoc()) {
         // Calcula a porcentagem de lucro (30%)
         $precoVenda = $row['price'];
@@ -42,11 +27,6 @@ if ($result) {
         echo '<td>' . $status . '</td>';
         echo '</tr>';
     }
-
-    echo '</tbody>';
-    echo '</table>';
-    echo '</div>';
-    echo '</div>';
 }
 
 $db->close();
