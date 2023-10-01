3c038eadb265088fd356bd54c270edcce5413c37
Marco Túlio Santana Nascimento <marco_nascimento@hst.br>
2023-10-01 11:31:22 -0300

    Try resolve fix in of row

--- api/DisplayProducts.php
+++ api/DisplayProducts.php
@@ -6,22 +6,21 @@ $result = $db->query($query);
 
 if ($result) {
     echo '<div class="card-body">';
-    
-        echo '<div class="table-responsive">';
-        while ($row = $result->fetch_assoc()) {
-            echo '<table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">';
-                echo '<thead>';
-                    echo '<tr>';
-                        echo '<th>Produto</th>';
-                        echo '<th>Categoria</th>';
-                        echo '<th>Preço de custo</th>';
-                        echo '<th>Preço de venda</th>';
-                        echo '<th>Porcentagem de lucro</th>';
-                        echo '<th>Ativo</th>';
-                    echo '</tr>';
-                echo '</thead>';
-        echo '<tbody>';
-        
+    echo '<div class="table-responsive">';
+    echo '<table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">';
+    echo '<thead>';
+    echo '<tr>';
+    echo '<th>Produto</th>';
+    echo '<th>Categoria</th>';
+    echo '<th>Preço de custo</th>';
+    echo '<th>Preço de venda</th>';
+    echo '<th>Porcentagem de lucro</th>';
+    echo '<th>Ativo</th>';
+    echo '</tr>';
+    echo '</thead>';
+    echo '<tbody>';
+
+    while ($row = $result->fetch_assoc()) {
         // Calcula a porcentagem de lucro (30%)
         $precoVenda = $row['price'];
         $porcentagemLucro = 30;
@@ -35,13 +34,13 @@ if ($result) {
         echo '<td>' . $porcentagemLucro . '%' . '</td>';
         echo '<td>' . $row['active'] . '</td>';
         echo '</tr>';
-        echo '</tbody>';
-        echo '</table>';
-        echo '</div>';
     }
+
+    echo '</tbody>';
+    echo '</table>';
     echo '</div>';
-} 
+    echo '</div>';
+}
 
 $db->close();
-
 ?>
