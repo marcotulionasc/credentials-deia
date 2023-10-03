9a70c09d9bbaa315082cc7ae2192c91b9102d3ff
Marco Túlio Santana Nascimento <marco_nascimento@hst.br>
2023-10-03 13:02:50 -0300

    testando 456

--- api/login.php
+++ api/login.php
@@ -4,10 +4,9 @@ require_once 'connection.php'; // Inclua seu arquivo de conexão
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $email = $_POST['email'];
-    $password = $_POST['password'];
 
-    // Consulta SQL para verificar as credenciais do usuário, incluindo o campo "token"
-    $query = "SELECT idAdmin, password FROM admin WHERE email = ?";
+    // Consulta SQL para verificar o email do usuário
+    $query = "SELECT idAdmin FROM admin WHERE email = ?";
     $stmt = $db->prepare($query);
     $stmt->bind_param("s", $email);
     $stmt->execute();
@@ -15,15 +14,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     if ($result->num_rows == 1) {
         $row = $result->fetch_assoc();
-        if (password_verify($password, $row['password'])) {
-            // Autenticação bem-sucedida
-            $_SESSION['admin_id'] = $row['idAdmin'];
-            header("Location: index.php");
-            exit;
-        } else {
-
-            echo "Credenciais inválidas.";
-        }
+        $_SESSION['admin_id'] = $row['idAdmin'];
+        header("Location: index.php");
+        exit;
     } else {
         echo "Email não encontrado.";
     }
