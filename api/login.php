bcf537c2660f2e6113a12881e956ec90ad852e3f
Marco Túlio Santana Nascimento <marco_nascimento@hst.br>
2023-10-03 12:46:06 -0300

    feature 2 of login

--- api/login.php
+++ api/login.php
@@ -5,10 +5,9 @@ require_once 'connection.php'; // Inclua seu arquivo de conexão
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $email = $_POST['email'];
     $password = $_POST['password'];
-    $token = $_POST['token'];
 
     // Consulta SQL para verificar as credenciais do usuário, incluindo o campo "token"
-    $query = "SELECT idAdmin, password, token FROM admin WHERE email = ?";
+    $query = "SELECT idAdmin, password FROM admin WHERE email = ?";
     $stmt = $db->prepare($query);
     $stmt->bind_param("s", $email);
     $stmt->execute();
@@ -16,19 +15,13 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     if ($result->num_rows == 1) {
         $row = $result->fetch_assoc();
-        if (password_verify($password, $row['password']) && $token === $row['token']) {
+        if (password_verify($password, $row['password'])) {
             // Autenticação bem-sucedida
             $_SESSION['admin_id'] = $row['idAdmin'];
             header("Location: index.php");
             exit;
         } else {
-            header("Location: index.php");
-            echo $email;
-            echo "<br>";
-            echo $password;
-            echo "<br>";
-            echo $token;
-            echo "<br>";
+
             echo "Credenciais inválidas.";
         }
     } else {
