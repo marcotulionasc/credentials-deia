5245f7df46884951abfcf88f988dc5921e6f5544
Marco Túlio Santana Nascimento <marco_nascimento@hst.br>
2023-10-31 11:33:59 -0300

    Voltando para nova estrutura, pois está funcionando na minha máquina

---
+++ api/Login.php
@@ -0,0 +1,29 @@
+<?php
+session_start();
+require_once 'connection.php'; // Inclua seu arquivo de conexão
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    
+    // Receber os dados do formulário
+    if (isset($_POST['email']) && isset($_POST['password'])) {
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+
+        // Consulta SQL para verificar o login
+        $sql = "SELECT * FROM admin WHERE email = '$email' AND password = '$password'";
+        $result = $db->query($sql);
+
+        if ($result->num_rows > 0) {
+            // Login correto
+            header("Location: DisplayProductsFront.php");
+        } else {
+            // Login incorreto
+            echo "Login incorreto. Verifique suas credenciais.";
+        }
+    } else {
+        echo "Por favor, preencha todos os campos do formulário.";
+    }
+
+    $db->close();
+}
+?>
