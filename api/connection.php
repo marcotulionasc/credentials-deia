b7a12925069fe42e900ff82dcd9c4c86faf7ae37
Marco Túlio Santana Nascimento <marco_nascimento@hst.br>
2023-11-15 16:44:36 -0300

    rc-1.0.0.4

--- api/connection.php
+++ api/connection.php
@@ -1,9 +1,15 @@
 <?php
 
-$hostname = "localhost";
-$username = "root";
-$password = "";
-$database = "deia";
-$db = new mysqli($hostname, $username, $password, $database);
+$DB_HOST = $_ENV["DB_HOST"];
+$DB_USER = $_ENV["DB_USER"];
+$DB_PASSWORD = $_ENV["DB_PASSWORD"];
+$DB_NAME = $_ENV["DB_NAME"];
+$DB_PORT = $_ENV["DB_PORT"];
+
+$db = new mysqli($DB_HOST, $DB_USER, $DB_PASSWORD, $DB_NAME, $DB_PORT);
+
+if ($db->connect_error) {
+    die("Falha na conexão com o banco de dados local e com o servidor: " . $db->connect_error);
+}
 
 ?>
